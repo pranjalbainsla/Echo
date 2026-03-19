2855790f94f7012129b8fbe188f3c9d50cd1e72f
Pranjal Bainsla <pbainsla18082004@gmail.com>
2026-03-19 23:04:17 +0530

    feat: add turborepo

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,13 +1,11 @@
 import { Button } from "@workspace/ui/components/button"
-import { Input } from "@workspace/ui/components/input"
 
 export default function Page() {
   return (
     <div className="flex min-h-svh p-6">
       <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
         <div>
-          <h1 className="font-medium">hello apps/web</h1>
-          <Input />
+          <h1 className="font-medium">Hello apps/widget</h1>
         </div>
       </div>
     </div>
