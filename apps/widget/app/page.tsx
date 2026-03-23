f1e034d78d3388573ad90f167fd6d55967ea4a6f
Pranjal Bainsla <pbainsla18082004@gmail.com>
2026-03-24 02:46:08 +0530

    05 : AI voice assistant

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,11 +1,29 @@
-import { Button } from "@workspace/ui/components/button"
+"use client";
+
+import { useVapi } from "@/modules/widget/hooks/use-vapi";
+import { Button } from "@workspace/ui/components/button";
 
 export default function Page() {
+  const { 
+        isConnected,
+        isConnecting,
+        isSpeaking,
+        transcript,
+        startCall,
+        endCall } = useVapi();
   return (
     <div className="flex min-h-svh p-6">
       <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
         <div>
-          <h1 className="font-medium">Hello apps/widget</h1>
+          <Button onClick={()=>startCall()}>Start Call</Button>
+          <Button onClick={()=>endCall()}>End Call</Button>
+          <div>
+            connection status: {isConnected ? "connected" : isConnecting ? "connecting..." : "disconnected"}
+          </div>
+          <div>
+            speaking status: {isSpeaking ? "speaking" : "not speaking"}
+          </div>
+          <div>{JSON.stringify(transcript, null, 2)}</div>
         </div>
       </div>
     </div>
---
+++ sentry.edge.config.ts
@@ -0,0 +1,20 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs"
+
+Sentry.init({
+  dsn: "https://3bdbf4acf97a55351ae8d40bdbd551d1@o4511094731440128.ingest.us.sentry.io/4511094744416256",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+})
