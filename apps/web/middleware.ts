738a96baa13cbf52acf8982943d64ec3557ce0f5
Pranjal Bainsla <pbainsla18082004@gmail.com>
2026-03-23 21:27:13 +0530

    04 : organizations

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,15 +1,41 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server';
 
 const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
-  "/sign-up(.*)",
-  "/test"
+  "/sign-up(.*)"
 ]);
 
+//for these routes, i dont want my middleware to redirect (inf loop)
+const isOrgFreeRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/org-selection(.*)"
+]);
 export default clerkMiddleware(async (auth, req)=> {
+  console.log("MIDDLEWARE RUNNING:", req.nextUrl.pathname);
+  const { userId, orgId } = await auth();
+
   if(!isPublicRoute(req)){
     await auth.protect();
   }
+
+  
+  // Logged in but NO org → force to org selection
+  // (allow org-selection itself to pass through)
+  if (
+    userId &&
+    !orgId &&
+    !req.nextUrl.pathname.startsWith('/org-selection')
+  ) {
+    return NextResponse.redirect(new URL('/org-selection', req.url))
+  }
+
+  // Has org but going to org-selection → redirect to dashboard
+  if (userId && orgId && req.nextUrl.pathname.startsWith('/org-selection')) {
+    return NextResponse.redirect(new URL('/', req.url))
+  }
+
 })
 
 export const config = {
